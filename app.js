e8603d2a2801979bc724c808593506b307f35665
Thomas Callaghan <callaghantw@gmail.com>
2020-05-21 19:02:21 -0400

    Animations going well. Email contact link working

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ new fullpage('#fullpage', {
         const h1anim = section.querySelector("h1");
         const panim = section.querySelector("p");
         const imganim = section.querySelector("img");
-        gsap.fromTo(h1anim, 1.5, {y: '50', opacity: 0}, {y: 0, opacity:1});
-        gsap.fromTo(panim, 1.5, {y: '50', opacity: 0}, {y: 0, opacity:1});
+        gsap.fromTo(h1anim, 1.5, {y: -50, opacity: 0}, {y: 0, opacity:1});
+        gsap.fromTo(panim, 1.5, {y: -50, opacity: 0}, {y: 0, opacity:1});
         gsap.fromTo(imganim, 1.5, {y: '-200', opacity: 0}, {y:-250, opacity:1});
     }
 });
