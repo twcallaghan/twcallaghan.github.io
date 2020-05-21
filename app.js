58b6cdbb4aec211281f7b8e8e49a522c0f773b93
Thomas Callaghan <callaghantw@gmail.com>
2020-05-21 17:46:43 -0400

    Messing around with links and pictures

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 new fullpage('#fullpage', {
-    navigation: true
+    afterLoad: (origin, destination, direction) => {
+        const section = destination.item;
+
+        const h1anim = section.querySelector("h1");
+        const panim = section.querySelector("p");
+        const imganim = section.querySelector("img");
+        gsap.fromTo(h1anim, 1.5, {y: '50', opacity: 0}, {y: 0, opacity:1});
+        gsap.fromTo(panim, 1.5, {y: '50', opacity: 0}, {y: 0, opacity:1});
+        gsap.fromTo(imganim, 1.5, {y: '-200', opacity: 0}, {y:-250, opacity:1});
+    }
 });
