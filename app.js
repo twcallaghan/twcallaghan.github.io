a1cd548ad97a00278ad49fe858a50c750f67a46d
Thomas Callaghan <callaghantw@gmail.com>
2020-05-22 19:32:37 -0400

    All pages are animated. Only have to clean up code and spellcheck everything now

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let edfirstload = false;
 let workfirstload = false;
 let projectsfirstload = false;
 let contactfirstload = false;
+let homefirstload = false;
 
 new fullpage('#fullpage', {
     navigation: true,
@@ -10,7 +11,7 @@ new fullpage('#fullpage', {
     anchors: ['homepage', 'educationpage', 'workexpage', 'projectspage', 'contactpage'],
 
 
-
+    // Loading the homepage if that is where the user starts
     afterLoad: (origin, destination, direction) => {
         const section = destination.item;
 
@@ -20,7 +21,8 @@ new fullpage('#fullpage', {
         const contactbtn = section.querySelector("button[id='contact-button']");
         const homebtn = section.querySelector("button[id='home-button']");
 
-        if (destination.index === 0 && origin.index === 0) {
+        if (destination.index === 0 && homefirstload === false) {
+            homefirstload = true;
             const h1anim = section.querySelector("h1");
             const panim = section.querySelector("p");
             const imganim = section.querySelector("img");
@@ -48,12 +50,29 @@ new fullpage('#fullpage', {
         const contactbtn = section.querySelector("button[id='contact-button']");
         const homebtn = section.querySelector("button[id='home-button']");
 
+        const h1anim = section.querySelector("h1");
+        const h3anim = section.querySelector("h3");
+        const imganim = section.querySelector("img");
+
+        // Going to the home page for the first time after starting on a different page (animations)
+        if (destination.index === 0 && homefirstload === false) {
+            homefirstload = true;
+            const panim = section.querySelector("p");
+
+            gsap.fromTo(h1anim, 1.5, {y: -50, opacity: 0}, {y: 0, opacity:1});
+            gsap.fromTo(panim, 1.5, {y: -50, opacity: 0}, {y: 0, opacity:1});
+            gsap.fromTo(imganim, 1.5, {y: 50, opacity: 0}, {y:0, opacity:1});
+
+            gsap.fromTo(edbtanim, 1.5, {y: 50, opacity: 0}, {y: 0, opacity:1});
+            gsap.fromTo(wrexbtn, 1.5, {y: 50, opacity: 0}, {y: 0, opacity:1});
+            gsap.fromTo(prjbtn, 1.5, {y: 50, opacity: 0}, {y: 0, opacity:1});
+            gsap.fromTo(contactbtn, 1.5, {y: 50, opacity: 0}, {y: 0, opacity:1});
+            gsap.fromTo(homebtn, 1.5, {y: 50, opacity: 0}, {y: 0, opacity:1});
+        }
+
+        // Going to Education page for the first time animations
         if (destination.index === 1 && edfirstload === false) {
             edfirstload = true;
-
-            const h1anim = section.querySelector("h1");
-            const h3anim = section.querySelector("h3");
-            const imganim = section.querySelector("img");
 
             gsap.fromTo(imganim, 1.5, {x: -50, opacity: 0}, {x: 0, opacity: 1, delay: 0.5});
             gsap.fromTo(h1anim, 1.5, {y: 50, opacity: 0}, {y: 0, opacity: 1, delay: 0.5});
@@ -66,7 +85,57 @@ new fullpage('#fullpage', {
             gsap.fromTo(homebtn, 1.5, {x: 50, opacity: 0}, {x: 0, opacity:1, delay: 0.5});
         }
 
-        
+        if (destination.index === 2 && workfirstload === false) {
+            workfirstload = true;
+
+            const imganim2 = section.querySelector("img[id='chef-pic']");
+            const h1anim2 = section.querySelector("h1[id='crunchtime-title']");
+
+            gsap.fromTo(imganim, 1.5, {y: -50, opacity: 0}, {y: 0, opacity:1, delay: 0.5});
+            gsap.fromTo(imganim2, 1.5, {y: -50, opacity: 0}, {y: 0, opacity:1, delay: 0.5});
+
+            gsap.fromTo(h1anim, 1.5, {y: 50, opacity: 0}, {y: 0, opacity:1, delay: 0.5});
+            gsap.fromTo(h1anim2, 1.5, {y: 50, opacity: 0}, {y: 0, opacity:1, delay: 0.5});
+            gsap.fromTo(h3anim, 1.5, {y: 50, opacity: 0}, {y: 0, opacity:1, delay: 0.5});
+
+            gsap.fromTo(edbtanim, 1.5, {y: -50, opacity: 0}, {y: 0, opacity:1, delay: 0.5});
+            gsap.fromTo(wrexbtn, 1.5, {y: -50, opacity: 0}, {y: 0, opacity:1, delay: 0.5});
+            gsap.fromTo(prjbtn, 1.5, {y: -50, opacity: 0}, {y: 0, opacity:1, delay: 0.5});
+            gsap.fromTo(contactbtn, 1.5, {y: -50, opacity: 0}, {y: 0, opacity:1, delay: 0.5});
+            gsap.fromTo(homebtn, 1.5, {y: -50, opacity: 0}, {y: 0, opacity:1, delay: 0.5});
+        }
+
+        if (destination.index === 3 && projectsfirstload === false) {
+            projectsfirstload = true;
+
+            gsap.fromTo(imganim, 1.5, {x: 50, opacity: 0}, {x: 0, opacity: 1, delay: 0.5});
+            gsap.fromTo(h1anim, 1.5, {y: -50, opacity: 0}, {y: 0, opacity: 1, delay: 0.5});
+            gsap.fromTo(h3anim, 1.5, {y: -50, opacity: 0}, {y: 0, opacity: 1, delay: 0.5});
+
+            gsap.fromTo(edbtanim, 1.5, {x: -50, opacity: 0}, {x: 0, opacity:1, delay: 0.5});
+            gsap.fromTo(wrexbtn, 1.5, {x: -50, opacity: 0}, {x: 0, opacity:1, delay: 0.5});
+            gsap.fromTo(prjbtn, 1.5, {x: -50, opacity: 0}, {x: 0, opacity:1, delay: 0.5});
+            gsap.fromTo(contactbtn, 1.5, {x: -50, opacity: 0}, {x: 0, opacity:1, delay: 0.5});
+            gsap.fromTo(homebtn, 1.5, {x: -50, opacity: 0}, {x: 0, opacity:1, delay: 0.5});
+        }
+
+        if (destination.index === 4 & contactfirstload === false) {
+            contactfirstload = true;
+            
+            const imganim2 = section.querySelector("img[id='linkedin-link-contact']");
+
+            gsap.fromTo(imganim, 1.5, {y: -50, x: -50, opacity: 0}, {y: 0, x: 0, opacity:1, delay: 0.5});
+            gsap.fromTo(imganim2, 1.5, {y: -50, x: -50, opacity: 0}, {y: 0, x: 0, opacity:1, delay: 0.5});
+
+            gsap.fromTo(h1anim, 1.5, {y: -50, opacity: 0}, {y: 0, opacity:1, delay: 0.5});
+            gsap.fromTo(h3anim, 1.5, {y: -50, opacity: 0}, {y: 0, opacity:1, delay: 0.5});
+
+            gsap.fromTo(edbtanim, 1.5, {x: 50, y: 50, opacity: 0}, {x: 0, y: 0, opacity:1, delay: 0.5});
+            gsap.fromTo(wrexbtn, 1.5, {x: 50, y: 50, opacity: 0}, {x: 0, y: 0, opacity:1, delay: 0.5});
+            gsap.fromTo(prjbtn, 1.5, {x: 50, y: 50, opacity: 0}, {x: 0, y: 0, opacity:1, delay: 0.5});
+            gsap.fromTo(contactbtn, 1.5, {x: 50, y: 50, opacity: 0}, {x: 0, y: 0, opacity:1, delay: 0.5});
+            gsap.fromTo(homebtn, 1.5, {x: 50, y: 50, opacity: 0}, {x: 0, y: 0, opacity:1, delay: 0.5});
+        }
     }
 
 });
